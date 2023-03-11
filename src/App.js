5de3e78df93b3f11c826fd2eb680460f18a61a1e
kmv100 <kononykhin.m@gmail.com>
2023-03-11 21:41:00 +0200

    added initial login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import Navbar from './Navbar';
-import Home from './Home';
+import React from "react";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import NotFound from "./404";
+import Login from "./Login";
+import useToken from "./useToken";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
+const App = () => {
+  // const token = getToken();
+  const { token, setToken } = useToken();
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            {/* <Route path='/blogs/:id' element={< BlogDetails />} /> */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
       </div>
     </div>
   );
